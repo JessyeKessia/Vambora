67270f02132c5c360ff08e77cc06429a785041a1
Danillo <coelho.danillo@academico.ifpb.edu.br>
2025-02-04 16:04:26 -0300

    alteração do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TelaLoginComponent } from "./tela-login/tela-login.component"
+import { TelaCadastroComponent } from "./tela-cadastro/tela-cadastro.component"
+import { TelaPrincipalComponent } from "./tela-principal/tela-principal.component"
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: 'Tela-Login', component: TelaLoginComponent},
+    {path: 'Tela-Cadastro', component: TelaCadastroComponent},
+    {path: 'Tela-Principal', component: TelaPrincipalComponent},
+
+];
---
+++ src/app/carona/carona.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carona',
+  imports: [],
+  templateUrl: './carona.component.html',
+  styleUrl: './carona.component.css'
+})
+export class CaronaComponent {
+
+}
