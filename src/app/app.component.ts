f24f367f1d040564ff8356faab6622353046ff88
danillo <coelho.danillo@academico.ifpb.edu.br>
2025-02-08 10:30:26 -0300

    Alterações final do cadastrar viagens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,49 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  standalone:false,
+  standalone: false,
 })
 export class AppComponent {
   title = 'VamboraPweb1';
+  isCollapsed = false;
+  itemSelecionado: number = -1; 
 
-  isCollapsed = false; 
-  itemSelecionado: number = -1;
+  constructor(private router: Router) {
+
+    this.atualizarSelecaoPorRota(this.router.url);
+
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.atualizarSelecaoPorRota(event.urlAfterRedirects);
+      }
+    });
+  }
 
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
   }
 
   selecionarItem(index: number, event: Event) {
-    event.stopPropagation(); 
+    event.stopPropagation();
     this.itemSelecionado = index;
+  }
+
+  private atualizarSelecaoPorRota(url: string) {
+    if (url.startsWith('/tela-principal')) {
+      this.itemSelecionado = 0;
+    } else if (url.startsWith('/listar-viagens')) {
+      this.itemSelecionado = 1;
+    } else if (url.startsWith('/cadastrar-viagem')) {
+      this.itemSelecionado = 2;
+    } else if (url.startsWith('/help')) {
+      this.itemSelecionado = 4;
+    } else {
+      this.itemSelecionado = -1; 
+    }
   }
 }
