099ec55618d4ba39f5fbd9126283c1f77c861a80
Danillo Coelho Barbosa <coelho.danillo@academico.ifpb.edu.br>
2025-02-15 07:00:09 -0600

    alterações na minha branch (sugestão)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListagemComponent} from "./carona/listagem/listagem.component";
 import {FormularioComponent} from "./carona/formulario/formulario.component";
+import {TelaPrincipalComponent} from "./tela-principal/tela-principal.component";
 
 
 const routes: Routes = [
+  {
+    path: '',
+    component: TelaPrincipalComponent,
+    pathMatch: 'full'
+  },
   {    
     path: 'minhascaronas',
     component: ListagemComponent
@@ -12,6 +18,10 @@ const routes: Routes = [
   {
     path: 'planejarviagens',
     component: FormularioComponent
+  },
+  {
+    path: 'tela-principal',
+    component: TelaPrincipalComponent
   }
 ];
 
